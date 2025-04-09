b9deb8d470fe4ed2b2bb8011d9e9e6bc19ba848e
Redisitic <rhythmjuneja7845@gmail.com>
2025-04-10 02:35:34 +0530

    kys

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "@/configs/firebase";
+
+function Landing() {
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const user = result.user;
+      console.log("Logged in as:", user.displayName);
+    } catch (error) {
+      console.error("Google login error:", error);
+    }
+  };
+  return (
+    <>
+        <Card className="mx-auto max-w-sm mt-[22vh]">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>Enter your email and password to login to your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" required />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <div className='text-center'>
+            or
+          </div>
+          <Button onClick={handleGoogleLogin} type="submit" className="w-full">
+            <img src="./google.svg" className='w-[2.5vh]'></img>
+            Login with google
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+    </>
+  )
+}
+
+export default Landing
